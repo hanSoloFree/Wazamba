4cf7fd6ec644905478476795998def0240b6349b
mac <artem.l@vortexads.com>
2021-12-16 18:01:05 +0200

    Added depending on level number.

--- Wazamba/GameScene.swift
+++ Wazamba/GameScene.swift
@@ -5,37 +5,78 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var background: SKSpriteNode!
     var ground: SKSpriteNode!
-    var blocksCount: Int = 10
+    var blocksCount: Int {
+        return countBlocks(level)
+    }
     var blocksArray: [SKSpriteNode] = [SKSpriteNode]()
     
-    //MARK: - MIX TIME
     
-    var mixTime = DispatchTime.now() + 2
+    //MARK: - LEVEL
+    var level: Int = 6
     
     var forced: Bool = false
     
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
+        physicsWorld.gravity.dy = gravity(self.level)
+        
+        print(blocksCount)
+        
         createBackground()
         createBlocks()
-        
-        print(self.frame.height / 64)
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
         let bodyA = contact.bodyA
         let bodyB = contact.bodyB
+
         
         if (bodyA.categoryBitMask == 0x1 << 0) && (bodyB.categoryBitMask == 0x1 << 0) {
             if !forced {
+                self.physicsWorld.gravity.dy = -9.8
                 let randomX = CGFloat.random(in: -50...50)
                 let impulse = CGVector(dx: randomX, dy: 50)
                 bodyA.node?.physicsBody?.applyImpulse(impulse)
                 bodyB.node?.physicsBody?.applyImpulse(impulse)
-                DispatchQueue.main.asyncAfter(deadline: mixTime) {
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
                     self.forced = true
                 }
             }
+        }
+    }
+    
+// MARK: - MAY BE INCAPSULATED
+    
+    func gravity(_ level: Int) -> CGFloat {
+        switch level {
+        case 1...3:
+            return -9
+        case 3...5:
+            return -5
+        case 6...7:
+            return -2
+        default:
+            return -9.8
+        }
+    }
+    func countBlocks(_ level: Int) -> Int {
+        switch level {
+        case 1:
+            return 2
+        case 2:
+            return 3
+        case 3:
+            return Int.random(in: 4...5)
+        case 4:
+            return 6
+        case 5:
+            return Int.random(in: 7...8)
+        case 6:
+            return 9
+        case 7:
+            return 10
+        default:
+            return 0
         }
     }
     
@@ -70,8 +111,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func createBlocks() {
-        //MARK: BLOCK
-        
+        //MARK: LINES OF BLOCKS
 
         if (blocksCount > 0) && (blocksCount <= 3) {
             buildLine(lineNumber: 1)
@@ -92,6 +132,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
     }
     
+    // MARK: LINE
     func buildLine(lineNumber: Int) {
         var blockPerLine: Int!
         var horizontalSpacing: CGFloat!
@@ -126,6 +167,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             break
         }
         
+        // MARK: BLOCK
+        
         for _ in 1...blockPerLine {
             index += 1
             let block = SKSpriteNode(imageNamed: String(describing: index))
@@ -145,6 +188,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             
             blocksArray.append(block)
             addChild(block)
+            
+            //MARK: BLOCK'S FRAME
+            
+            let frame = SKSpriteNode(imageNamed: "frame")
+            frame.size = CGSize(width: blockHeight + 10, height: blockHeight + 10)
+            frame.position = CGPoint(x: x, y: y)
+            frame.zPosition = 9
+            addChild(frame)
         }
     }
     
