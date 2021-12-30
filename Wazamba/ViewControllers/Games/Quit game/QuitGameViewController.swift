cfe0370fe4136f61641932cdf079f73b6c0b1d3c
mac <artem.l@vortexads.com>
2021-12-30 18:10:00 +0200

    //

---
+++ Wazamba/ViewControllers/Games/Quit game/QuitGameViewController.swift
@@ -0,0 +1,39 @@
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class QuitGameViewController: BaseViewController, GameOverDelegate {
+    
+    var won: Bool?
+    var level: Int = 3
+    var currentLevel: Int?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let view = self.view as! SKView? {
+            if let scene = SKScene(fileNamed: "QuitGameScene") as? QuitGameScene {
+                
+                scene.level = level
+                scene.gameOverDelegate = self
+                scene.scaleMode = .resizeFill
+                view.presentScene(scene)
+            }
+        }
+    }
+    
+    func pushGameOverViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let gameOverViewController = storyboard.instantiateViewController(withIdentifier: "GameOverViewController") as? GameOverViewController else { return }
+        
+        gameOverViewController.game = "chain"
+        gameOverViewController.won = self.won
+        gameOverViewController.currentLevel = self.currentLevel
+        self.navigationController?.pushViewController(gameOverViewController, animated: true)
+    }
+    
+}
